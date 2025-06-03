d0e227e3defd1611ccc97aa85bedc74289918406
chamodi1234 <chamodichethana1234@gmail.com>
2025-06-03 08:34:38 +0530

    updated

--- server/routes/logRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { getLogs } = require('../controllers/logController');
-const router = express.Router();
-
-router.get('/', getLogs); // Ensure this route is being set up correctly
-
-module.exports = router;
