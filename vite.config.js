49ac392d3700f29b4b39c008d20c9b52cd394f0d
SHAKILA DISSANAYAKE <129902002+Shakilmadu@users.noreply.github.com>
2025-06-05 13:43:02 +0530

    init

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    allowedHosts: 'all' // Allow any host
+    allowedHosts: true // Allow any host
   }
 })
